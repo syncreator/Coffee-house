e5bcb6a5a0b50dd3557e3e0680d45e3455145b68
Syncreator <51112090+syncreator@users.noreply.github.com>
2024-02-07 13:24:49 +0200

    Update data.js

--- menu/data.js
+++ menu/data.js
@@ -14,6 +14,7 @@ const renderCart = (img, name, about, price) => {
       <p class="cart-info-name">${name}</p>
       <p class="cart-info-about">${about}</p>
     </div>
+    <span>$</span>
     <p class="cart-info-price">${price}</p>
   </div>
   `
